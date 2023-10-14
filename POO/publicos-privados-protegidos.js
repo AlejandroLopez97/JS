2de49739ea7c610d85f4afd23162b8325c1e323f
SoporteAdmin <Soporteadmin@PASCUALBRAVO.CENTRAL>
2023-10-13 19:05:23 -0500

    Se adicionan getter and setter

--- POO/publicos-privados-protegidos.js
+++ POO/publicos-privados-protegidos.js
@@ -19,13 +19,17 @@ class Persona {
         console.log(`Hola, mi nombre es ${this.#nombre}, tengo ${this.#edad} años`);
     }
 
-    obtenNombre(){
+    getNombre(){
         return this.#nombre;
     }
 
     //Metodos privados
-    #obtenEdad(){
+    getEdad(){
         return this.#edad;
+    }
+
+    setEdad(nuevaEdad){
+        this.#edad = nuevaEdad;
     }
 }
 
@@ -33,9 +37,13 @@ const persona = new Persona('Alejo', 80);
 console.log(persona.nombre);
 console.log(persona.edad);
 persona.saludo();
-persona.obtenNombre();
+persona.getNombre();
 persona._isDeveloper;
 
 //Desde el exterior de la clase si no hago la clase privada
 //pueden realizarse cambios
 
+//Getter --> metodos que nos permiten obtener atributos/metodos privados o protegidos
+const edad = persona.getEdad();
+//Setter --> metodos que nos permiten asignar atributos/metodos privados o protegidod
+persona.setEdad(35);
